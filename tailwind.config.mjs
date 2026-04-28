da24be8dd85cca9d49e7b7cff307cd7f125ace1d
RamonLGDaw <ramonlg.daw@gmail.com>
2026-04-28 21:57:48 +0200

    npx tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdb,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
